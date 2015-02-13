6bfae02e0da7c36fd2d2ec466e928f6628ea7406
arometh <contact@arometh.com>
2015-02-13 18:55:22 +0100

    Echeance-vendredi-13022015-1 : Mise à jour du code

--- EcheanceStock/trunk/src/main/java/sn/arometh/notification/job/GestionNotificationStockJob.java
+++ EcheanceStock/trunk/src/main/java/sn/arometh/notification/job/GestionNotificationStockJob.java
@@ -1,5 +1,7 @@
 package sn.arometh.notification.job;
 
+import java.sql.SQLException;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
@@ -7,9 +9,9 @@ import org.apache.log4j.Logger;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.StatefulJob;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import sn.arometh.notification.bean.GestionNotificationStockBean;
 import sn.arometh.notification.business.Business;
 import sn.arometh.notification.commons.ConstantFunctionnals;
 import sn.arometh.notification.commons.SendMail;
@@ -18,12 +20,28 @@ public class GestionNotificationStockJob extends QuartzJobBean implements Statef
 
     private static Logger logger = Logger.getLogger(GestionNotificationStockJob.class);
                                                     
-    @Autowired
     private Business bussiness;
     
-    @Override
+    private GestionNotificationStockBean gestionNotificationBean;
+    
+    
+    public GestionNotificationStockJob() {
+    	logger.debug("############ Constructeur: Creation d'un nouveau bean pour la gestion des Notification.");
+    	gestionNotificationBean = new GestionNotificationStockBean();
+    	try {
+			bussiness = new Business(false);
+		} catch (ClassNotFoundException e) {
+			logger.error("ERROR", e);
+			e.printStackTrace();
+		} catch (SQLException e) {
+			logger.error("ERROR", e);
+			e.printStackTrace();
+		}
+	}
+
+	@Override
     protected void executeInternal(JobExecutionContext pArg0) throws JobExecutionException {
-        logger.debug("############ Constructeur: Creation d'un nouveau bean de gestion des Stock .");
+        logger.debug("############ Execution du scheduler de gestion des notifications => " + gestionNotificationBean.getNameScheduler());
         String stockNotification = bussiness.rechercherStockAlert();        
         if(Boolean.getBoolean(VAR_NOTIFICATION_CONFIGURATION_STOP_MAIL)){
            //envoie de mail
